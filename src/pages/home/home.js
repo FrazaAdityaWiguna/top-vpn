499e925944f994befb585b3abed4c3aedf28f11a
FrazaAdityaWiguna <adityafraja12@gmail.com>
2022-05-21 07:23:20 +0700

    up to github

---
+++ src/pages/home/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="container max-w-screen-xl mx-auto 2xl:max-w-screen-2xl p-6">
+      <div class="flex justify-between">
+        <span className="logo">
+          Top<span className="font-bold">VPN</span>
+        </span>
+
+        <span className="name">Fraza Aditya Wiguna</span>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
